30da7ceed71b3988e1dd80aadb8862b9882c8222
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-20 12:12:53 -0400

    second commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,23 +3,52 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+    openDB('contacts', 1, {
+        upgrade(db) {
+          if (db.objectStoreNames.contains('contacts')) {
+            console.log('contacts database already exists');
+            return;
+          }
+          db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+          console.log('contacts database created');
+        },
+    });
 };
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+    console.log('Post to the ase');
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts');
+    const request = store.add({ name, home, cell, email });
+    const result = await request;
+    console.log('Data saved to the database', result);
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+    console.log('GET all from the database');
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readonly');
+    const store = tx.objectStore('contacts');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+    console.log('DELETE from the database', id);
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts');
+    const request = store.delete(id);
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { GenerateSW } = require('workbox-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 module.exports = () => {
   return {
@@ -11,16 +15,63 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new GenerateSW({
+        swDest: 'service-worker.js'
+      }),
+      new WebpackPwaManifest({
+        // TODO: Create a manifest.json:
+        name: 'My progressive Web App',
+        short_name: 'MyPwa',
+        description: "Keep track of important tasks!",
+        start_url: './',
+        display: 'standalone',
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+            
+          }
+        ],
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
 
     }
   };
